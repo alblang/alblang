0c8e8433b500a1f9a2216db6c0e80a5468a92d17
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-17 22:05:00 +0200

    starting a different aproach usin parser combinator library and my microlisp implementation

---
+++ src/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mpc.h"
+
+#include "parser.h"
+
+#ifdef _WIN32
+
+  #include <string.h>
+
+  static char buffer[2048];
+
+  /* Fake readline function */
+  char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+
+    cpy[strlen(ctr)-1] = '\0';
+    
+    return cpy;
+  }
+
+  /* Fake add_history function */
+  void add_history(char* unused) {}
+
+#else
+
+  #include <editline/readline.h>
+
+#endif
+
+void init() {
+  puts("alb v0.0.1");
+  puts("Ctrl + c to exit.");
+}
+
+int main(int argc, char** argv) {
+  
+  lenv* e = lenv_new();
+  lenv_add_builtins(e);
+
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Symbol   = mpc_new("symbol");
+  mpc_parser_t* Sexpr    = mpc_new("sexpr");
+  mpc_parser_t* Qexpr    = mpc_new("qexpr");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Microlisp = mpc_new("microlisp");
+
+  /* Define them with the following Language */
+  mpca_lang(MPC_LANG_DEFAULT,
+    "                                                      \
+      number : /-?[0-9]+/ ;                                \
+      symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;          \
+      sexpr  : '(' <expr>* ')' ;                           \
+      qexpr  : '{' <expr>* '}' ;                           \
+      expr   : <number> | <symbol> | <sexpr> | <qexpr> ;   \
+      microlisp  : /^/ <expr>* /$/ ;                       \
+    ",
+    Number, Symbol, Sexpr, Qexpr, Expr, Microlisp);
+  init();
+  
+  while(1) {
+    char* input = readline("µL >> ");
+
+    add_history(input);
+    
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Microlisp, &r)) {      
+      lval* x = lval_eval(e, lval_read(r.output));
+      lval_println(x);
+      lval_del(x);
+      
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
+    free(input);
+  }
+
+  /* Undefine and Delete our Parsers */
+  mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Microlisp);
+  
+  return 0;
+}
---
+++ src/parser.h
@@ -0,0 +1,67 @@
+/* Functions interfaces */
+#include <stdlib.h>
+
+/* Forward Declarations */
+struct lval;
+struct lenv;
+typedef struct lval lval;
+typedef struct lenv lenv;
+
+/* Lisp Value */
+enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_FUN, LVAL_SEXPR, LVAL_QEXPR };
+
+typedef lval*(*lbuiltin)(lenv*, lval*);
+
+lval* lval_num(long x);
+lval* lval_err(char* fmt, ...);
+lval* lval_sym(char* s);
+lval* lval_fun(lbuiltin func);
+lval* lval_sexpr(void);
+lval* lval_qexpr(void);
+
+void  lval_del(lval* v);
+lval* lval_copy(lval* v);
+lval* lval_add(lval* v, lval* x);
+lval* lval_join(lval* x, lval* y);
+lval* lval_pop(lval* v, int i);
+lval* lval_take(lval* v, int i);
+
+void lval_print(lval* v);
+void lval_print_expr(lval* v, char open, char close);
+void lval_println(lval* v);
+
+char* ltype_name(int t);
+
+/* Lisp Environment */
+struct lenv;
+
+lenv* lenv_new(void);
+void  lenv_del(lenv* e);
+lval* lenv_get(lenv* e, lval* k);
+void  lenv_put(lenv* e, lval* k, lval* v);
+
+lval* builtin_list(lenv* e, lval* a);
+lval* builtin_head(lenv* e, lval* a);
+lval* builtin_tail(lenv* e, lval* a);
+lval* builtin_eval(lenv* e, lval* a);
+lval* builtin_join(lenv* e, lval* a);
+
+lval* builtin_op(lenv* e, lval* a, char* op);
+lval* builtin_add(lenv* e, lval* a);
+lval* builtin_sub(lenv* e, lval* a);
+lval* builtin_mul(lenv* e, lval* a);
+lval* builtin_div(lenv* e, lval* a);
+lval* builtin_def(lenv* e, lval* a);
+
+void lenv_add_builtin(lenv* e, char* name, lbuiltin func);
+void lenv_add_builtins(lenv* e);
+
+/* Evaluation */
+
+lval* lval_eval_sexpr(lenv* e, lval* v);
+lval* lval_eval(lenv* e, lval* v);
+
+/* Reading */
+
+lval* lval_read_num(mpc_ast_t* t);
+lval* lval_read(mpc_ast_t* t);
