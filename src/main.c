b0bcb860ece797fd1fc4f3b577c4f9e691a5752e
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-17 22:12:15 +0200

    fixing compilation problem after refactoring

--- src/main.c
+++ src/main.c
@@ -5,32 +5,7 @@
 
 #include "parser.h"
 
-#ifdef _WIN32
-
-  #include <string.h>
-
-  static char buffer[2048];
-
-  /* Fake readline function */
-  char* readline(char* prompt) {
-    fputs(prompt, stdout);
-    fgets(buffer, 2048, stdin);
-    char* cpy = malloc(strlen(buffer)+1);
-    strcpy(cpy, buffer);
-
-    cpy[strlen(ctr)-1] = '\0';
-    
-    return cpy;
-  }
-
-  /* Fake add_history function */
-  void add_history(char* unused) {}
-
-#else
-
-  #include <editline/readline.h>
-
-#endif
+#include <editline/readline.h>
 
 void init() {
   puts("alb v0.0.1");
@@ -69,7 +44,7 @@ int main(int argc, char** argv) {
     add_history(input);
     
     mpc_result_t r;
-    if (mpc_parse("<stdin>", input, Microlisp, &r)) {      
+    if (mpc_parse("<stdin>", input, Alblang, &r)) {      
       lval* x = lval_eval(e, lval_read(r.output));
       lval_println(x);
       lval_del(x);
