3c19c5a9c2e72b9a40df39b0c4be1a1724b9c8dd
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-21 22:16:04 +0200

    spliting lines of code to understand better

--- src/parser.c
+++ src/parser.c
@@ -628,26 +628,35 @@ void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
 void lenv_add_builtins(lenv* e) {
   /* Variable Functions */
   lenv_add_builtin(e, "\\",  builtin_lambda); 
-  lenv_add_builtin(e, "def", builtin_def); lenv_add_builtin(e, "=", builtin_put);
+  lenv_add_builtin(e, "def", builtin_def); 
+  lenv_add_builtin(e, "=",   builtin_put);
   
   /* List Functions */
   lenv_add_builtin(e, "list", builtin_list);
-  lenv_add_builtin(e, "head", builtin_head); lenv_add_builtin(e, "tail",  builtin_tail);
-  lenv_add_builtin(e, "eval", builtin_eval); lenv_add_builtin(e, "join",  builtin_join);
+  lenv_add_builtin(e, "head", builtin_head); 
+  lenv_add_builtin(e, "tail", builtin_tail);
+  lenv_add_builtin(e, "eval", builtin_eval); 
+  lenv_add_builtin(e, "join", builtin_join);
   
   /* Mathematical Functions */
-  lenv_add_builtin(e, "+",    builtin_add); lenv_add_builtin(e, "-",     builtin_sub);
-  lenv_add_builtin(e, "*",    builtin_mul); lenv_add_builtin(e, "/",     builtin_div);
+  lenv_add_builtin(e, "+",    builtin_add); 
+  lenv_add_builtin(e, "-",    builtin_sub);
+  lenv_add_builtin(e, "*",    builtin_mul); 
+  lenv_add_builtin(e, "/",    builtin_div);
   
   /* Comparison Functions */
   lenv_add_builtin(e, "if",   builtin_if);
-  lenv_add_builtin(e, "==",   builtin_eq); lenv_add_builtin(e, "!=",   builtin_ne);
-  lenv_add_builtin(e, ">",    builtin_gt); lenv_add_builtin(e, "<",    builtin_lt);
-  lenv_add_builtin(e, ">=",   builtin_ge); lenv_add_builtin(e, "<=",   builtin_le);
+  lenv_add_builtin(e, "==",   builtin_eq); 
+  lenv_add_builtin(e, "!=",   builtin_ne);
+  lenv_add_builtin(e, ">",    builtin_gt); 
+  lenv_add_builtin(e, "<",    builtin_lt);
+  lenv_add_builtin(e, ">=",   builtin_ge); 
+  lenv_add_builtin(e, "<=",   builtin_le);
   
   /* String Functions */
   lenv_add_builtin(e, "load", builtin_load); 
-  lenv_add_builtin(e, "error", builtin_error); lenv_add_builtin(e, "print", builtin_print); 
+  lenv_add_builtin(e, "error", builtin_error); 
+  lenv_add_builtin(e, "print", builtin_print); 
 }
 
 /* Evaluation */
