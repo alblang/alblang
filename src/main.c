00441eaf76ea6f6cd25ea5d16d4ab1e7e89be3ae
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-28 22:41:47 +0200

    starting from scratch, firts parsers

--- src/main.c
+++ src/main.c
@@ -6,39 +6,20 @@
 
 #include <editline/readline.h>
 
-void init() {
-  puts("alblang v0.1");
-  puts("Ctrl + c to exit.");
-}
-
 int main(int argc, char** argv) {
   
-  lenv* e = lenv_new();
-  lenv_add_builtins(e);
-
   create_parsers();
-  
-  /* Interactive Prompt */
-  if (argc == 1) {
-    init();
-  
-    while(1) {
-      char* input = readline(":>  ");
+    
+  init();
 
-      add_history(input);
-      
-      parser_execute(e, input);
+  while(1) {
+    char* input = readline(":>  ");
 
-      free(input);
-    }
+    add_history(input);
+    
+    parser_execute(input);
 
-
-  } 
-  
-  /* Supplied with list of files */
-  if (argc >= 2) {
-  
-    parser_execute_files(e, argc, argv);
+    free(input);
   }
 
   cleanup_parsers();
