6d04d5d56701e76a5d070bee4653a91a4566e259
Jesús Navarrete <jesus.navarrete@gmail.com>
2014-04-29 14:08:55 +0200

    removing commented code

--- src/main.c
+++ src/main.c
@@ -13,17 +13,10 @@ int main(int argc, char** argv) {
   init();
 
   if (argc > 1) {
-    /*mpc_result_t r;
-    if (mpc_parse_contents(argv[1], Alblang, &r)) {
-      mpc_ast_print(r.output);
-      mpc_ast_delete(r.output);
-    } else {
-      mpc_err_print(r.error);
-      mpc_err_delete(r.error);
-    }*/
-    parser_execute_file(argv[1]);
 
-  } else {
+    parser_execute_file(argv[1]);
+  
+  } else {    
     while(1) {
       char* input = readline(":>  ");
 
