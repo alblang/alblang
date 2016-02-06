439f8dbfd4497bbd73a5b0c3810697df8ee09f99
Jesús Navarrete <jesus.navarrete@gmail.com>
2016-02-06 16:58:04 +0100

    moving mpc parser to parser folder and update with the new version 0.8.7 06/02/2016

--- src/main.c
+++ src/main.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "mpc.h"
+#include "parser/mpc.h"
 #include "parser.h"
 
 #include <editline/readline.h>
 
 int main(int argc, char** argv) {
-  
-  create_parsers();    
-  
+
+  create_parsers();
+
   init();
 
   if (argc > 1) {
 
     parser_execute_file(argv[1]);
-  
-  } else {    
+
+  } else {
     while(1) {
       char* input = readline(":>  ");
-      
+
       add_history(input);
-      
+
       if (strcmp(input, "") == 0) {
         continue;
-      } else if (strcmp(input, "exit") == 0) {               
+      } else if (strcmp(input, "exit") == 0) {
         break;
       } else {
         parser_execute(input);
